7d9d933aad5d481f1a7028710e114a2350ed3778
[Jonas-eng-21] <92036392+Jonas-eng-21@users.noreply.github.com>
2022-07-31 20:25:41 -0300

    Conclusão do login do usuário

--- server/index.js
+++ server/index.js
@@ -1,23 +1,39 @@
 const express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser')
 
 const port = 3000;
 var path = require('path');
 const app = express();
 
-app.use(session({secret: 'chavealeatoria'}))
+var login = "admin";
+var password = "123456"
 
+app.use(session({secret: 'chavealeatoria'}))
+app.use(bodyParser.urlencoded({extended:true}));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
 
 app.post('/',(req, res)=>{
-  res.render('index');
+
+  if(req.body.password == password && req.body.login == login){
+    //Logado com sucesso!
+    req.session.login = login;
+    res.render('logado',{login: login});
+  }else{
+    res.render('index');
+  }
 })
 
 app.get('/', (req, res)=>{
-  res.render('index');
+  if(req.session.login){  //Se existir o login, será redirecionado para uma página de logado
+    req.render('logado', {login: login});
+    // console.log('O meu usuário logado é: '+  req.session.login);
+  }else{  //Caso não exista será redirecionado para o index inicial
+    res.render('index');
+  }
 })
 
 app.listen(port, ()=>{
