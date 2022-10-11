da0f97e86e9862a5672a749d0c284a3e13348daf
[Jonas-eng-21] <92036392+Jonas-eng-21@users.noreply.github.com>
2022-10-11 19:41:47 -0300

    Merge branch 'main'

--- mobile/App.js
+++ mobile/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Login from './screens/Login';
+import Login from './screens/Login/Login';
 
 // expo start --tunnel
 
