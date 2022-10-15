6a340be896c0b097ae12690f2339bd55a178e3d0
Emilio <emiliogabriel000@gmail.com>
2022-10-15 10:27:33 -0300

    backend atualizado e requisições prontas para serem feitas via api

--- mobile/src/Pages/Cadastro/index.tsx
+++ mobile/src/Pages/Cadastro/index.tsx
@@ -11,11 +11,26 @@ function Cadastro() {
   const [password, setPassword] = useState<string>("");
 
   function handleSignUpClick() {
+   
       api.post("auth/register", {
           name: name,
           email: email,
-          senha: password,
-      })
+          password: password,
+      },).catch(function (error) {
+        if (error.response) {
+          // Request made and server responded
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+    
+      });
   }
 
   
--- mobile/src/Services/api.ts
+++ mobile/src/Services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/"
+    baseURL: "http://192.168.18.17:3000/"
 })
+
+
 
 export default api
